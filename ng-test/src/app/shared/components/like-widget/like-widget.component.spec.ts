cea090e477e0aa45cb23d393bf048317832e5b68
Daniel Guerra <danielguerra.g12@gmail.com>
2021-06-22 17:29:35 -0300

    Criado primeira etapa de testes do component.

--- ng-test/src/app/shared/components/like-widget/like-widget.component.spec.ts
+++ ng-test/src/app/shared/components/like-widget/like-widget.component.spec.ts
@@ -1,25 +1,53 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 
-import { LikeWidgetComponent } from './like-widget.component';
+import { UniqueIdService } from "../../services/unique-id/unique-id.service";
+import { LikeWidgetComponent } from "./like-widget.component";
+import { LikeWidgetModule } from "./like-widget.module";
 
-describe('LikeWidgetComponent', () => {
-  let component: LikeWidgetComponent;
-  let fixture: ComponentFixture<LikeWidgetComponent>;
+describe(LikeWidgetComponent.name, () => {
+  let fixture: ComponentFixture<LikeWidgetComponent> = null;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LikeWidgetComponent ]
-    })
-    .compileComponents();
-  });
+      imports: [LikeWidgetModule]
+    }).compileComponents();
 
-  beforeEach(() => {
     fixture = TestBed.createComponent(LikeWidgetComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('Should create component', () => {
+    const instance = fixture.componentInstance;
+    expect(instance).toBeTruthy();
   });
+
 });
+
+/**
+ * Em runtime, um component precisa pertencer a um módulo e em ambiente de teste
+ * também será necessário. O TestBed é responsável por criar um módulo para meu
+ * component em ambiente de teste.
+ *
+ * A cadeia de dependência profunda, isso é, quando nosso component tem mais de 
+ * uma dependência, nos traz vários problemas pois seria o desenvolvedor 
+ * responsável por testar todas elas.
+ *
+ * Quando trabalhamos com Angular, em momento algum instanciamos um component com
+ * "new". Tudo é criado automaticamente pelo framework de forma de declarativa.
+ * Por isso o Angular já cuida de tratar o ciclo de vida de cada component. Ao
+ * instanciar com "new", perdemos essa possibilidade.
+ *
+ * O TestBed possui um método chamado configureTestingModule que cria o módulo 
+ * para teste. É criado um declaration para importar o component. O bloco do 
+ * beforeEach retorna uma promise e por isso é necessário o async/await para que 
+ * essa chamada seja resolvida. O TestBed também passa sem async/await, porém 
+ * desta forma é independente do builder (webpack) que estou usando e caso haja
+ * alguma atualização, isso impede de que o teste pare de funcionar. 
+ *
+ * O ComponentFixture é um rapper responsável por embrulhar o componente e dar 
+ * uma série de métodos que serão usados para testar. 
+ *
+ *
+ *
+ */
